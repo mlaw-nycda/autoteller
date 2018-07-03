85c19abf6241d975d46c0ad2d5468b360b42aa16
Michael Law <michael.law@nycda.com>
2018-07-03 15:05:59 -0400

    learned javascript loops to dynamically create images

--- application.js
+++ application.js
@@ -1,1 +1,21 @@
-console.log('Javascript is working');
+var saiyans = [
+	'gohan.png',
+	'goku.jpg',
+	'nappa.png',
+	'vegeta.jpg',
+	'trunks.jpg'
+]
+
+var gallery = document.getElementById('gallery');
+
+var count = 0;
+for (thisItem in saiyans) {
+	// print img tag
+	// set img src = "images/" + saiyans[count]
+	var newImage = document.createElement('img');
+	newImage.src = "images/" + saiyans[count];
+	newImage.classList.add("responsive");
+	newImage.classList.add("image-container");
+	gallery.appendChild(newImage);
+	count = count + 1;
+}
